6ac9131367fbe4aa2a8b1267e99b256543fad7eb
chparkNotebook <kim759900@naver.com>
2024-05-12 22:08:16 +0900

    create npc base

---
+++ TDProject/Source/TDProject/NPCs/TDNPCBase.cpp
@@ -0,0 +1,26 @@
+#include "NPCs/TDNPCBase.h"
+#include "TDNPCBase.h"
+
+ATDNPCBase::ATDNPCBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ATDNPCBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATDNPCBase::Move()
+{
+	//캐릭터에게 다가가도록
+}
+
+void ATDNPCBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ TDProject/Source/TDProject/NPCs/TDNPCBase.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TDNPCBase.generated.h"
+
+UCLASS()
+class TDPROJECT_API ATDNPCBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ATDNPCBase();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	UFUNCTION()
+	void Move();
+
+};
+
+
+//기본 NPC는 움직임이 곧 공격이다.
